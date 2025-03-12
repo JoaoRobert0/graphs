409082a0702bc2a5aa8a238b2f3c4f7654cce007
JoaoRobert0 <devjoaoroberto@gmail.com>
2025-03-12 19:37:50 -0300

    add more atributes to Vertex class

--- source/adjacencylist/Vertex.java
+++ source/adjacencylist/Vertex.java
@@ -3,10 +3,18 @@ package source.adjacencylist;
 public class Vertex {
     public String label;
     public boolean visited;
+    public String color;
+    public Vertex father;
+    public int distance;
+
+    public static final int INFINITY = Integer.MAX_VALUE; 
 
     public Vertex(String label) {
         this.label = label;
         this.visited = false;
+        this.color = "white";
+        this.father = null;
+        this.distance = INFINITY;
     }
 
     @Override
